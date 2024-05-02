830ac87970b382266fe5817a2f571bff5e5d9083
EldarSalakhov <seven.mo@yandex.ru>
2024-05-02 20:16:14 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Что куда копировать?: $0 <директория-источник> <директория-назначение>"
+    echo "Что куда копировать?: $0 < директория - источник > < директория - назначение >"
     exit 1
 fi
 
-source_dir="$1"
-dest_dir="$2"
+s="$1"
+d="$2"
 
-mkdir -p "$dest_dir"
+mkdir -p "$d"
 
-for file in $(find "$source_dir" -type f); do
-    base_name=$(basename "$file")
-    target_file="$dest_dir/$base_name"
+for f in $(find "$s" -type f); do
+    b=$(basename "$f")
+    t="$d/$b"
     
-    if [ -f "$target_file" ]; then
-        num=1
-        while [ -f "$dest_dir/${base_name%.*}_$num.${base_name##*.}" ]; do
-            num=$((num + 1))
+    if [ -f "$t" ]; then
+        n=1
+        while [ -f "$d/${b%.*}_$n.${b##*.}" ]; do
+            n=$((n + 1))
         done
-        target_file="$dest_dir/${base_name%.*}_$num.${base_name##*.}"
+        t="$d/${b%.*}_$n.${b##*.}"
     fi
     
-    cp "$file" "$target_file"
+    cp "$f" "$t"
 done
 
-echo "Файлы успешно скопированы из $source_dir в $dest_dir."
+echo "Файлы успешно скопированы из $s в $d."
